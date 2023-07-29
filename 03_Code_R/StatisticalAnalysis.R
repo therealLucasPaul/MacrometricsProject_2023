b7b50e659536ba076857de2f2bd0e54e3bfad551
Fynn Lohre <129409649+VARFynn@users.noreply.github.com>
2023-07-29 17:27:47 +0200

    To Avoid Merge Issues
    Not FInal for today

--- 03_Code_R/StatisticalAnalysis.R
+++ 03_Code_R/StatisticalAnalysis.R
@@ -20,8 +20,8 @@ mod_extensive <- lm(pct2021 ~ dist+Nato+BRICS+OKVS+continent+DirectBorder+Second
 summary(mod_extensive)
 
 ##### Bayesian Analysis
-#### Model 1 - Every Covariate
-fit.mod1 <- brm(pct2021 ~ dist+Nato+BRICS+OKVS+continent+DirectBorder+SecondaryBorder,
+#### Model 1 - Every Covariate 
+fit.mod1 <- brm(pct2021 ~ dist+Nato+BRICS+OKVS+continent+DirectBorder+SecondaryBorder+electdem_vdem_owid,
                 data = totaldata,
                 prior = c(prior(normal(0, 1), class = b),
                           prior(inv_gamma(2, 1), class = sigma)),
@@ -37,20 +37,9 @@ mcmc_areas(fit.mod1, pars = parnames(fit.mod1)[6:9], prob=0.95) + ggtitle("Poste
 mcmc_areas(fit.mod1, pars = parnames(fit.mod1)[3:5], prob=0.95) + ggtitle("Posterior Distribution of the Alliances Parameters (Model 1)") + theme_minimal() + geom_vline(xintercept=0, color="red", lty = "dashed", size=1.2) + ylab("Parameters")
 mcmc_areas(fit.mod1, pars = parnames(fit.mod1)[10:11], prob=0.95) + ggtitle("Posterior Distribution of the Border Relationship Parameters (Model 1)") + theme_minimal() + geom_vline(xintercept=0, color="red", lty = "dashed", size=1.2) + ylab("Parameters")
 
-#### Model 2 - Only Distance
-fit.mod2 <- brm(pct2021 ~ dist,
-                data = totaldata,
-                prior = c(prior(normal(0, 1), class = b),
-                          prior(inv_gamma(2, 1), class = sigma)),
-                save_pars = save_pars(all = TRUE),
-                sample_prior="yes",
-                chains=2,
-                iter=1000,
-                warmup = 500)
-mcmc_areas(fit.mod2, pars = c("b_dist"), prob=0.95) + ggtitle("Posterior Distribution of the Distance Parameter (Model 2)")  + theme_minimal() + geom_vline(xintercept=0, color="red", lty = "dashed", size=1.2) + ylab("Parameters")+xlim(-.0004,.00005)
 
-#### Model 3 - No basic distance measure
-fit.mod3 <- brm(pct2021 ~ Nato+BRICS+OKVS+continent+DirectBorder+SecondaryBorder,
+#### Model 2 - Every Covariate except electdem_vdem_owid ("just physical distance")
+fit.mod2 <- brm(pct2021 ~ dist+Nato+BRICS+OKVS+continent+DirectBorder+SecondaryBorder,
                 data = totaldata,
                 prior = c(prior(normal(0, 1), class = b),
                           prior(inv_gamma(2, 1), class = sigma)),
@@ -59,7 +48,38 @@ fit.mod3 <- brm(pct2021 ~ Nato+BRICS+OKVS+continent+DirectBorder+SecondaryBorder
                 chains=2,
                 iter=1000,
                 warmup = 500)
-mcmc_areas(fit.mod3, pars = parnames(fit.mod3)[1:10], prob=0.95) + ggtitle("Posterior Distribution of all Parameters (Model 3)")  + theme_minimal() + geom_vline(xintercept=0, color="red", lty = "dashed", size=1.2) + ylab("Parameters")
+fit.mod2
+coefs_mod2 <- fixef(fit.mod1)
+mcmc_areas(fit.mod2, pars = c("b_dist"), prob=0.95) + ggtitle("Posterior Distribution of the Distance Parameter (Model 2)")  + theme_minimal() + geom_vline(xintercept=0, color="red", lty = "dashed", size=1.2) + ylab("Parameters")+xlim(-.0004,.00005)
+mcmc_areas(fit.mod2, pars = parnames(fit.mod2)[6:9], prob=0.95) + ggtitle("Posterior Distribution of the Continent Parameters (Model 2)")  + theme_minimal() + geom_vline(xintercept=0, color="red", lty = "dashed", size=1.2) + ylab("Parameters")
+mcmc_areas(fit.mod2, pars = parnames(fit.mod2)[3:5], prob=0.95) + ggtitle("Posterior Distribution of the Alliances Parameters (Model 2)") + theme_minimal() + geom_vline(xintercept=0, color="red", lty = "dashed", size=1.2) + ylab("Parameters")
+mcmc_areas(fit.mod2, pars = parnames(fit.mod2)[10:11], prob=0.95) + ggtitle("Posterior Distribution of the Border Relationship Parameters (Model 2)") + theme_minimal() + geom_vline(xintercept=0, color="red", lty = "dashed", size=1.2) + ylab("Parameters")
+
+#### Model 3 - Only Distance
+fit.mod3 <- brm(pct2021 ~ dist,
+                data = totaldata,
+                prior = c(prior(normal(0, 1), class = b),
+                          prior(inv_gamma(2, 1), class = sigma)),
+                save_pars = save_pars(all = TRUE),
+                sample_prior="yes",
+                chains=2,
+                iter=1000,
+                warmup = 500)
+mcmc_areas(fit.mod3, pars = c("b_dist"), prob=0.95) + ggtitle("Posterior Distribution of the Distance Parameter (Model 3)")  + theme_minimal() + geom_vline(xintercept=0, color="red", lty = "dashed", size=1.2) + ylab("Parameters")+xlim(-.0004,.00005)
+
+#### Model 4 - No Distance
+fit.mod4 <- brm(pct2021 ~ Nato+BRICS+OKVS+continent+DirectBorder+SecondaryBorder,
+                data = totaldata,
+                prior = c(prior(normal(0, 1), class = b),
+                          prior(inv_gamma(2, 1), class = sigma)),
+                save_pars = save_pars(all = TRUE),
+                sample_prior="yes",
+                chains=2,
+                iter=1000,
+                warmup = 500)
+mcmc_areas(fit.mod4, pars = parnames(fit.mod4)[1:10], prob=0.95) + ggtitle("Posterior Distribution of all Parameters (Model 4)")  + theme_minimal() + geom_vline(xintercept=0, color="red", lty = "dashed", size=1.2) + ylab("Parameters")
+
+
 
 #### Model Weights
-model_weights(fit.mod1, fit.mod2, fit.mod3, weights = "waic")
+model_weights(fit.mod1, fit.mod2, fit.mod3, fit.mod4, weights = "waic")
